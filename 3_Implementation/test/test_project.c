71929b8244e396780682ab0d51817125da78d16e
Deepika <deepi24kcm@gmail.com>
2022-03-02 19:24:57 +0530

    commit

--- 3_Implementation/test/test_project.c
+++ 3_Implementation/test/test_project.c
@@ -27,21 +27,20 @@ int main(int argc, char **argv)
     UNITY_BEGIN();
     RUN_TEST(test_cancel);
     UNITY_END();
-    printf("\t\t\t***AIRLINE BOOKING***\t\t\t\t\n");
-	int ch =0;
+  	int ch =0;
 	while(ch!=4)
 	{
-	printf("\n\nDo you want to - \n 1. Reserve a ticket? \n 2. Cancel Booking \n 3. Display passenger details \n 4. exit\n\n");
+	printf("\n\nPlease enter your choice from below (1-4):\n 1. Reservation \n 2. Cancel \n 3. DISPLAY RECORDS\n 4. EXIT\n feel free to ask us\n Enter your choose");
 	scanf("%d", &choice);
 	switch(choice)
 	{
 		case 1 :  status=reserve(start);
 	              if(status==0)
-	                printf("\nBooking Full!! \nYou are added to waiting list. Waiting list number %d", count);
+	                printf("\nSeat full %d", count);
 	              else
-	                printf(" \nBooking Successful!!! Enjoy your journey! Your Reg No is %d\n\n", num);
+	                printf(" \nSeat booking successful! \nyour seat number is: %d\n\n", num);
 	              break;
-	    case 2:   printf(" \n Give the Registration number to be cancelled\n");
+	    case 2:   printf(" \n Enter passport number to delete record?:\n");
 	              scanf(" %d", &reg);
 	              if(reg>num)
 	              printf("Invalid!!");
@@ -49,16 +48,16 @@ int main(int argc, char **argv)
 	              {
 	              canc=cancel(reg);
 	              if(canc==-1)
-	              	printf("\nRegistration number invalid!!\n");
+	              	printf("\nPassport number is invalid!!\n");
 	              else
-	              	printf("\nRegistration cancelled successfully\n");
+	              	printf("\nhas been deleted\n");
 	              	}
 	              break;
 	    case 3: display();
 	    break;
 	    case 4: exit(0);
 	    break;
-	    default: printf("\nWrong choice!\n");
+	    default: printf("\n SORRY INVALID CHOOICE! \n PLEASE CHOOSE FROM 1-4 \n Do not forget to choose from 1-4 \n");
 }
 }
 }
